4970d7ea4a5e2583c5b6bd4c5104f30a16ded017
prakashbabu11 <prakash.babu20695@gmail.com>
2023-01-12 20:51:53 +0530

    Initial files2

--- NZWalks/NZWalks.API/Controllers/WeatherForecastController.cs
+++ NZWalks/NZWalks.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace NZWalks.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freez", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freeze", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
