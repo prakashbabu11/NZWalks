52f8080de3ccc68f56269b0e5ddceebed304a3d2
prakashbabu11 <prakash.babu20695@gmail.com>
2023-01-17 21:52:38 +0530

    End of section 2 of web api.

---
+++ NZWalks/NZWalks.API/NewFolder/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.NewFolder.Domain
+{
+    public class Region
+    {
+        public Guid id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+        //Navigate Property
+
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/NewFolder/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.NewFolder.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
